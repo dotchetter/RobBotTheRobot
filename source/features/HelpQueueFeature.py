ad711aa08a72b551142c03d8f7511ca23c92e797
dotchetter <dotchetter@protonmail.ch>
2020-04-24 01:22:14 +0200

    Implemented a new queue for presentations only. It has the same options as the help queue, but makes it easier for students and teacher to enqueue for specific reasons which alleviates in priorities.

--- source/features/HelpQueueFeature.py
+++ source/features/HelpQueueFeature.py
@@ -20,7 +20,9 @@ class HelpQueueFeature(ci.FeatureBase):
 
     def __init__(self, *args, **kwargs):
         self.help_queue = Queue()
-        self.latest_queue_state = 0
+        self.demonstration_queue = Queue()
+
+
         self.command_parser = HelpQueueFeatureCommandParser()
         self.command_parser.keywords = HelpQueueFeature.FEATURE_KEYWORDS
         
@@ -28,18 +30,24 @@ class HelpQueueFeature(ci.FeatureBase):
             CommandPronoun.INTERROGATIVE,
             CommandPronoun.UNIDENTIFIED
         )
-
+        
         self.command_parser.callbacks = {
-            'mig': self.enqueue,
-            'next': self.dequeue,
-            'nästa': self.dequeue,
-            'kö': lambda: self.list_queue(),
-            'kön': lambda: self.list_queue()
+            str({'visa': ('kö', 'kön', 'hjälp')}): lambda: self.list_help_queue(),
+            str({'visa': ('redovisning', 'redovisa')}): lambda: self.list_demonstration_queue(),
+            str({'hjälp': ('mig',)}): self.enqueue,
+            str({'help': ('mig', 'me')}): self.enqueue,
+            str({'hjälp': ('nästa', 'next')}): self.dequeue,
+            str({'jag': ('redovisa',)}): self.demonstrate_enqueue,
+            str({'redovisa': ('nästa', 'next')}): self.demonstrate_dequeue,
+            'hjälp': self.enqueue,
+            'redovisa': self.demonstrate_enqueue
         }
 
         self.command_parser.interactive_methods = (
             self.enqueue,
             self.dequeue,
+            self.demonstrate_enqueue,
+            self.demonstrate_dequeue
         )
 
         super().__init__(
@@ -88,10 +96,10 @@ class HelpQueueFeature(ci.FeatureBase):
         return f'{message.author.mention}, du saknar behörighet för detta'
 
     @logger
-    def list_queue(self) -> str:
+    def list_help_queue(self) -> str:
         """
         Returns a concatenated string with all the members in 
-        queue, with their place in the queue as leading digit.
+        help queue, with their place in the queue as leading digit.
         """
         output = []
         if not self.help_queue.queue:
@@ -100,7 +108,19 @@ class HelpQueueFeature(ci.FeatureBase):
             output.append(f"‧ {place + 1}: `{member.name.strip('@')}`")
         return f'{os.linesep.join(output)}'
 
-    def queue_went_active(self) -> str:
+    @logger
+    def list_demonstration_queue(self) -> str:
+        """
+        Returns a concatenated string with all the members in 
+        demonstration queue, with their place in the queue as 
+        leading digit.
+        """
+        output = []
+        if not self.demonstration_queue.queue:
+            return 'Listan för redovisningar är tom'
+        for place, member in enumerate(self.demonstration_queue.queue):
+            output.append(f"‧ {place + 1}: `{member.name.strip('@')}`")
+        return f'{os.linesep.join(output)}'
         """
         This method returns a phrase if the queue size went
         from 0 to 1 in size since last call. It can be
@@ -112,4 +132,43 @@ class HelpQueueFeature(ci.FeatureBase):
         """
         if self.latest_queue_state == 0 and self.help_queue.qsize() == 1:
             return ':warning: Hjälplistan är aktiv'
-        self.latest_queue_state = self.help_queue.qsize()
+        self.latest_queue_state = self.help_queue.qsize()    @logger
+    @logger
+    def demonstrate_enqueue(self, message: discord.Message) -> str:
+        """
+        When this method is called, the author of any
+        given parameter message is enqueued into the 
+        demonstration queue, held separeate from the
+        help queue structure.
+        :param message:
+            discord.Message object from chat
+        :returns:
+            str
+        """
+        for n, i in enumerate(self.demonstration_queue.queue):
+            if i == message.author: 
+                return f'{message.author.mention} du står redan i kön på plats {n + 1}'
+        self.demonstration_queue.put(message.author)
+        return f'{message.author.mention} skrevs upp. Du har plats {self.demonstration_queue.qsize()}'
+
+    @logger
+    def demonstrate_dequeue(self, message: discord.Message) -> str:
+        """
+        This method dequeues the next user in line
+        for presenting their work to the teacher. 
+        Dequeing is limited to members with the 
+        "teacher" role only, this is checked first before
+        dequeueing.
+        :param message:
+            discord.Message, the whole message object
+            from the chat application
+        """
+        if not self.demonstration_queue.qsize():
+            return 'Listan för presentationer är tom'
+
+        try:
+            if len([i for i in message.author.roles if i.name == 'teacher']):
+                return f'Näst på kö för att presentera är {self.demonstration_queue.get().mention}'
+        except AttributeError:
+            return f'Du kan bara använda detta kommando i en av kanalerna, inte i PM'
+        return f'{message.author.mention}, du saknar behörighet för detta'
